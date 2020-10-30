dc2aa6f935ccd5107776131f079603472679283a
agga1 <agud100@gmail.com>
2020-10-30 12:52:11 +0100

    no conflicts, if only with else, no break/continue/return

--- scanner.py
+++ scanner.py
@@ -98,41 +98,33 @@ def p_statements(p):
                  | statements any_statement
                 """
 
-def p_statement_assign(p):
-    """statement : ID '=' expression"""
-    names[p[1]] = p[3]
-
 def p_any_statements(p):
     """any_statement : no_semi_statement
                     | statement ';' """
 
 def p_no_semi_statements(p):
-    """no_semi_statement : if """
-    # | while todo
-    # | for
+    """no_semi_statement : if
+                         | while
+                         | for"""
 
 def p_statement_expr(p):
     """statement : expression
                 """
-                # | if
-    # """
-    # print(p[1])
-
-def p_expression_print(p):
+def p_statement_print(p):
     """statement : PRINT expression"""
-    print(p[2])
+
+def p_statement_assign(p):
+    """statement : id_whole_or_part '=' expression """
+
+def p_id_whole_or_part(p):
+    """id_whole_or_part : ID
+                        | id_partial """
 
 def p_statement_update(p):
-    """statement :  ID '[' index_ref ']' '=' expression
-                 | ID '[' index_ref_pair ']' '=' expression"""
-    try:
-        M = names[p[1]]
-        if isinstance(p[3], tuple):
-            M[p[3][0], p[3][1]] = p[6]
-        else:
-            M[p[3]] = p[6]
-    except LookupError:
-        print(f"Undefined name {p[1]!r}")
+    """statement : id_whole_or_part ADDASSIGN expression
+                | id_whole_or_part SUBASSIGN expression
+                | id_whole_or_part DIVASSIGN expression
+                | id_whole_or_part MULASSIGN expression"""
 
 # EXPRESSION -------------------------------------
 def p_expression_binop(p):
@@ -144,125 +136,68 @@ def p_expression_binop(p):
                   | expression DOTDIV expression
                   | expression DOTSUB expression
                   | expression DOTMUL expression"""
-    p1 = expression_factory(p[1])
-    p2 = expression_factory(p[3])
-    p[0] = p1.eval(p[2], p2)
-    # print("result", p[0], p[2] )
+
 
 def p_expression_uminus(p):
     """expression : '-' expression %prec UMINUS"""
-    p[0] = -p[2]
 
 def p_expression_number(p):
     '''expression : number
-                  | matrix
                   | id_partial
-                  | row
+                  | list
                   | STR
                   | logical
                   '''
-    p[0] = p[1]
 
 def p_expression_name(p):
     """expression : ID"""
-    try:
-        p[0] = names[p[1]]
-    except LookupError:
-        print(f"Undefined name {p[1]!r}")
-        p[0] = 0
+
 
 def p_expression_group(p):
     """expression : '(' expression  ')' """
-    p[0] = p[2]
 
 def p_zeros(p):
     """expression : ZEROS '(' INTNUM ')'"""
-    p[0] = np.zeros((p[3], p[3]))
 
 
 def p_ones(p):
     """expression : ONES '(' INTNUM ')'"""
-    p[0] = np.ones((p[3], p[3]))
 
 
 def p_eye(p):
     """expression : EYE '(' INTNUM ')'"""
-    p[0] = np.eye(p[3])
 
 
 def p_expression_transpose(p):
     """expression : expression "\'" """
-    p1 = ExpressionList(p[1])
-    p[0] = p1.transpose()
 
 # NUMERICAL ------------------------------------------------
 def p_number(p):
     """number : INTNUM
-                | FLOAT """
-    p[0] = p[1]
+              | FLOAT """
 
-# MATRIX ops ------------------------------------------------
-
-
+# INDEX_REF ------------------------------------------------
 def p_range(p):
     """range : INTNUM ':' INTNUM"""
-    p[0] = [i for i in range(p[1], p[3])]
 
 def p_index_ref(p):
     """index_ref : INTNUM
-                | range """
-    p[0] = p[1]
-
-def p_index_ref_pair(p):
-    """index_ref_pair : index_ref ',' index_ref """
-    p[0] = ( p[1], p[3] )
+                | range
+                | index_ref ',' INTNUM
+                | index_ref ',' range"""
 
 def p_matrix_part(p):
-    """id_partial : ID '[' index_ref ']'
-                    |  ID '[' index_ref_pair ']'"""
-    try:
-        M = names[p[1]]
-        if isinstance(p[3], tuple):
-            p[0] = M[p[3][0], p[3][1]]
-        else:
-            p[0] = M[p[3]]
-    except LookupError:
-        print(f"Undefined name {p[1]!r}")
-        p[0] = 0
+    """id_partial : ID '[' index_ref ']' """
 
-def p_matrix(p):
-    """matrix : '[' row_list ']' """
-    p[0] = np.array(p[2])
-
-def p_matrix_extend(p):
-    """row_list : row_list ','  row
-    """
-    p[0] = p[1]
-    p[0].append(p[3])
-
-def p_matrix_first(p):
-    """row_list : row
-    """
-    p[0] = [ p[1] ]
-#
-# # ROW -----------------------------------------------------
-def p_row(p):
-    """row : '[' value_list ']' """
-    p[0] = np.array(p[2])
+# # LIST -----------------------------------------------------
+def p_list(p):
+    """list : '[' value_list ']' """
 
 def p_num_list_extend(p):
-    """value_list : value_list ','  number
+    """value_list : expression
+                  | value_list ','  expression
     """
-    p[0] = p[1]
-    p[0].append(p[3])
-
-def p_num_list_first(p):
-    """value_list : number
-    """
-    p[0] = [ p[1] ]
-
-# # LOGICAL ops -------------------------------------------------
-
+# LOGICAL -------------------------------------------------
 def p_logical(p):
     """logical : expression EQ expression
                 | expression LT expression
@@ -271,26 +206,35 @@ def p_logical(p):
                 | expression GTE expression
                 | expression DIFF expression
     """
-    if p[2] == t_EQ  : p[0] = p[1] == p[3]
-    elif p[2] == t_LT  : p[0] = p[1] < p[3]
-    elif p[2] == t_GT  : p[0] = p[1] > p[3]
-    elif p[2] == t_LTE : p[0] = p[1] <= p[3]
-    elif p[2] == t_GTE : p[0] = p[1] >= p[3]
-    elif p[2] == t_DIFF: p[0] = p[1] != p[3]
 
+# IF  -------------------------------------------------
 def p_if(p):
-    """if : IF '(' logical ')' any_statement
-          | IF '(' logical ')' '{' statements '}'
+    """if : IF '(' logical ')' any_statement else_block
+          | IF '(' logical ')' '{' statements '}' else_block
     """
-    if p[3]:
-        # exp = p[6] if p[5] == '{' else p[5]
-        print("true")
-        # p[0] = exp
-    else:
-        print("false")
+    print("got if")
 
+def p_else(p):
+    """else_block : ELSE any_statement
+                  | ELSE '{' statements '}'
+    """
+    print("got else")
 
+def p_while(p):
+    """while : WHILE '(' logical ')' any_statement
+          | WHILE '(' logical ')' '{' statements '}'
+    """
+
+def p_for(p):
+    """for : FOR ID '=' range any_statement
+          | FOR ID '=' range '{' statements '}'
+    """
+
+# ERROR -------------------------------------------------
 def p_error(p):
     print("Syntax error in input!")
+
+# def p_empty(p):
+#     'empty :'
 
 parser = yacc.yacc()
