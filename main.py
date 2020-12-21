6bcc430d5568df88db24ed5cc85d698f15d92679
agga1 <agud100@gmail.com>
2020-12-21 18:37:13 +0100

    [EDIT] main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from TypeChecker import TypeChecker
 if __name__ == '__main__':
     # -------from file
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "examples/TypeCheck1.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "examples/TypeCheck3.txt"
         file = open(filename, "r")
     except IOError:
         print("Cannot open {0} file".format(filename))
@@ -18,13 +18,3 @@ if __name__ == '__main__':
 
     typeChecker = TypeChecker()
     typeChecker.visit(ast)
-
-    # ------interactive console
-    print("-----------------------------------")
-    print("console mode -- type 'q' to quit ")
-    parser = mparser.parser
-    s = input('scanner >')
-    while(s != 'q'):
-        res = parser.parse(s)
-        res.print_tree()
-        s = input('scanner >')
